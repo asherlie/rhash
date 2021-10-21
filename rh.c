8cf0e6b3f08474cae1b1a558911a27ce6eb6db78
asherlie <lieber.asher@gmail.com>
2021-10-21 00:24:44 -0400

    insert_rhash() is now functional

--- rh.c
+++ rh.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #if 0
 recursively defined hashing structure
 can have O(1)...ish lookup time with minimal memory footprint
@@ -49,41 +50,102 @@ void insert_ll(struct ll* l, int val, void* data){
 
 struct rhash{
     /* it's left up to the user to define hashing functions that */
-    int (*hash_func) (int, void*);
+    /*int (*hash_func) (int, void*);*/
     /* n_bux will be set to -1 on the final layer */
-    int n_bux;
+    /*int n_bux;*/
     /* each entry either has n_bux more entries or a linked list at the final layer */
 
     union{
-        struct rhash* buckets;
+        struct rhash** buckets;
         /*struct ll_entry* list;*/
         struct ll* list;
     };
 };
 
-void init_rhash(struct rhash* rh, int levels, int n_bux, int (**hfuncs)(int, void*)){
-    struct rhash* rhp = rh;
+struct rhash_container{
+    int n_bux;
+    int levels;
+    int (**hash_funcs) (int, void*);
+    struct rhash rh;
+};
+
+/*
+ * [a, b, c, d, e, f, g]
+ * 
+ * a = [x, y, z, xx, yy]
+*/
+
+void init_rhash(struct rhash_container* rhc, int levels, int n_bux, int (**hfuncs)(int, void*)){
+    struct rhash* rh = &rhc->rh;
+
+    rhc->hash_funcs = hfuncs;
+    rhc->levels = levels;
+    rhc->n_bux = n_bux;
+    
+    /*rh->n_bux = n_bux;*/
+    /*rh->buckets = calloc(sizeof(struct rhash*), rh->n_bux);*/
+    rh->buckets = NULL;
+
+    #if 0
     for(int i = 0; i < levels; ++i){
         rhp->n_bux = n_bux;
         /* TODO: should buckets only be allocated when it's time for that space to be used? */
-        rhp->buckets = calloc(sizeof(struct rhash), rhp->n_bux);
+        rhp->buckets = calloc(sizeof(struct rhash*), rhp->n_bux);
+        printf("created level %i bucket at %p\n", i, (void*)rhp->buckets);
         rhp->hash_func = hfuncs[i];
         rhp = rhp->buckets;
     }
     rhp->n_bux = -1;
     rhp->list = NULL;
+    #endif
 }
 
+
+
+void insert_rhash(struct rhash_container* rhc, int val, void* data){
+    struct rhash* rh = &rhc->rh, * tmp; 
+    int tmp_hash;
+
+    for(int i = 0; i < rhc->levels; ++i){
+        if(!rh->buckets)rh->buckets = calloc(sizeof(struct rhash*), rhc->n_bux);
+        tmp_hash = rhc->hash_funcs[i](val, data);
+        /*printf("laid foundation at level %i: %p\n", i, (void*)rh->buckets);*/
+        /*rh->n_bux = 20;*/
+        /* filling in the actual rhash */
+        if(!(tmp = rh->buckets[tmp_hash])){
+            rh->buckets[tmp_hash] = calloc(sizeof(struct rhash), 1);
+            /*printf("inserted an rhash at %p\n", (void*)rh->buckets[tmp_hash]);*/
+        }
+        rh = rh->buckets[tmp_hash];
+        /*rh->n_bux = 20;*/
+        /*
+         * if(!(tmp = rh->buckets[tmp_hash])){
+         *     rh->buckets[tmp_hash] = calloc(sizeof(struct rhash*), 20);
+         *     rh->n_bux = 20;
+         *     printf("inserted level %i at %p\n", i, (void*)rh);
+         *     rh = rh->buckets[tmp_hash];
+         * }
+        */
+    }
+
+    if(!rh->list)rh->list = calloc(1, sizeof(struct ll));
+    insert_ll(rh->list, val, data);
+}
+#if 0
 void insert_rhash(struct rhash* rh, int val, void* data){
     struct rhash* rhp = rh;
     while(rhp->n_bux != -1){
+        printf("rhp->buckets: %p\n", (void*)rhp->buckets);
         rhp = &rhp->buckets[rhp->hash_func(val, data)];
     }
+
+    insert_ll(rhp->list, val, data);
     /*
      * if(!data){
      * }
     */
 }
+#endif
 
 int example_hash_0(int val, void* data){
     (void)data;
@@ -101,7 +163,7 @@ int example_hash_2(int val, void* data){
 }
 
 int main(){
-    struct rhash rh;
+    struct rhash_container rhc;
     int (**hfuncs)(int, void*) = malloc(sizeof(hfuncs)*5);
     hfuncs[0] = example_hash_0;
     hfuncs[1] = example_hash_1;
@@ -109,7 +171,10 @@ int main(){
     hfuncs[3] = example_hash_0;
     hfuncs[4] = example_hash_1;
 
-    init_rhash(&rh, 5, 20, hfuncs);
+    init_rhash(&rhc, 5, 20, hfuncs);
+
+    insert_rhash(&rhc, 32, NULL);
+    insert_rhash(&rhc, 32, NULL);
 
     return 0;
 }
