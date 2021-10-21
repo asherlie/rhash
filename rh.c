e65f5e0ffbc0f46fb526c384ce2c67d7164cdd55
asherlie <lieber.asher@gmail.com>
2021-10-20 23:05:39 -0400

    cleaned up code

--- rh.c
+++ rh.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #if 0
 recursively defined hashing structure
 can have O(1)...ish lookup time with minimal memory footprint
@@ -39,20 +40,19 @@ struct rhash{
     /* each entry either has n_bux more entries or a linked list at the final layer */
     union{
         struct rhash* buckets;
-        struct ll* list;
+        struct ll_entry* list;
     };
 };
 
-void init_rhash(struct rhash* rh, int levels){
+void init_rhash(struct rhash* rh, int levels, int n_bux){
     struct rhash* rhp = rh;
     for(int i = 0; i < levels; ++i){
         rhp->n_bux = n_bux;
         rhp->buckets = calloc(sizeof(struct rhash), rhp->n_bux);
-        rhp->final_layer = 0;
         rhp = rhp->buckets;
     }
-    rhp->final_layer = 1;
     rhp->n_bux = -1;
+    rhp->list = NULL;
 }
 
 int main(){
